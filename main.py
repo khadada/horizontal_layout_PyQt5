d72c0a8c2df7df09a75d7e1fbe4a02b1db283961
khadada <lkhadada@gmail.com>
2022-04-15 07:27:23 +0100

    All done with initalize_ui method

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget,QHBoxLayout)
 from custom_widget_placeholder import Color
 
-class MainWindow(QMainWindow)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+        self.show()
+    
+    def initialize_ui(self):
+        """
+        Initialize the main window and display its content
+        """
+        self.setWindowTitle("Horizontal Layout")
+        self.setGeometry(100,50, 800, 600)
+        self.display_widgets()
+    
