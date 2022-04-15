56aff50a99b753db131f27156269a65493b25b35
khadada <lkhadada@gmail.com>
2022-04-15 07:19:56 +0100

    I forget to import the QHBoxLayout. lol

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Import necessary modules and classes:
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget,QHBoxLayout)
 from custom_widget_placeholder import Color
+
+class MainWindow(QMainWindow)
