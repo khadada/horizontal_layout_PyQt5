c7cb485e9b452dd780caeb71b23ffb865dd0fcdf
khadada <lkhadada@gmail.com>
2022-04-15 07:39:54 +0100

    All done with dispaly_widgets method with comments

--- main.py
+++ main.py
@@ -17,3 +17,21 @@ class MainWindow(QMainWindow):
         self.setGeometry(100,50, 800, 600)
         self.display_widgets()
     
+    def display_widgets(self):
+        """
+        Display the color widgets on the main window.
+        """
+        # Create the layout:
+        h_layout = QHBoxLayout()
+        # Create the container:
+        container = QWidget()
+        # Create instances of Color class & add it to the layout:
+        h_layout.addWidget(Color("#EF0087"))
+        h_layout.addWidget(Color("#6945DB"))
+        h_layout.addWidget(Color("#34E453"))
+        h_layout.addWidget(Color("#FF633A"))
+        # Set the container to use the layout:
+        container.setLayout(h_layout)
+        # Set the container as the central widget for the main window:
+        self.setCentralWidget(container)
+    
